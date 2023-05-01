0dd0bc14fa5404d1bd055c52a4f86a909125c82e
Eden Vidal <edenvidal@gmail.com>
2023-05-01 12:21:26 +0300

    Update rnbw-map.js

--- rnbw-map.js
+++ rnbw-map.js
@@ -2,41 +2,31 @@ const rnbwMapTemplate = `
                 <div
                     class="direction-column box hidden-on-mobile border radius-s padding-m border gap-m"
                     style="min-height: 600px">
-                    <div
-                        class="direction-row panel justify-stretch gap-xl background-primary radius-s border">
+                    <div id="anim4" class="direction-row panel justify-stretch gap-xl background-primary radius-s border">
                         <div>
                             <div class="border-bottom padding-m">
                                 <a>
-                                    <span class="text-l">1.2 Navigate</span>
+                                    <span class="text-l"><span class="padding-xs background-secondary radius-s border">1.2</span> Navigate</span>
                                     <p class="text-s">Navigate anywhere.</p>
                                 </a>
                             </div>
                             <div class="border-bottom padding-m">
-                                <a href="#workspaces">
-                                    <span class="text-l">1.3 Workspaces</span>
+                                <a href="https://guide.rnbw.dev/quick-start#workspaces">
+                                    <span class="text-l">
+                                        <span class="padding-xs background-secondary radius-s border">1.3</span> Workspaces/Projects/Files
+                                    </span>
                                     <p class="text-s">
-                                        View and toggle workspaces.
-                                    </p>
-                                </a>
-                                <a href="#projects">
-                                    <span class="text-l">1.3 Projects</span>
-                                    <p class="text-s">
-                                        View and toggle projects.
-                                    </p>
-                                </a>
-                                <a href="#files">
-                                    <span class="text-l">1.3 Files</span>
-                                    <p class="text-s">
-                                        View, edit, and organize your files.
+                                        View and toggle workspaces, projects and
+                                        their files.
                                     </p>
                                 </a>
                             </div>
 
                             <div class="border-bottom padding-m box">
-                                <a href="#elements">
-                                    <span class="text-l"
-                                        >1.6 Elements / Tree</span
-                                    >
+                                <a href="https://guide.rnbw.dev/quick-start#elements">
+                                    <span class="text-l">
+                                        <span class="padding-xs background-secondary radius-s border">1.6</span> Elements
+                                    </span>
                                     <p class="text-s">
                                         View and control objects in a
                                         hierarchical manner
@@ -45,8 +35,7 @@ const rnbwMapTemplate = `
                             </div>
                         </div>
                         <div>
-                            <div
-                                class="padding-m gap-s border-bottom opacity-l">
+                            <div class="padding-m gap-s border-bottom opacity-l">
                                 <a>
                                     <span class="text-l">Settings</span>
                                     <p class="text-s">
@@ -67,46 +56,48 @@ const rnbwMapTemplate = `
                         </div>
                     </div>
                     <div class="box direction-row gap-m">
-                        <a href="#elements">
-                            <span class="text-l">1.6 Elements / Stage</span>
+                    <svg-icon class="opacity-xl" src="https://rnbw.company/images/shapes.svg"></svg-icon>
+                        <a id="anim2" href="https://guide.rnbw.dev/quick-start#elements">
+                            <span class="text-l"><span class="padding-xs background-secondary radius-s border">1.6</span> Stage</span>
                             <p class="text-s">
                                 Visualize and manipulate objects and their
                                 composition.
                             </p>
                         </a>
-                        <div
-                            class="padding-l radius-s background-primary border">
-                            <a href="#jumpstart">
-                                <span class="text-l">1.1 Jumpstart</span>
-                                <p class="text-s">
-                                    Start and open projects, control settings
-                                    and get help.
-                                </p>
-                            </a>
+                        <div id="anim1" class="border radius-s box-s align-stretch" style="align-self: center;">
+                            <div class="padding-l border-bottom">
+                                <a href="https://guide.rnbw.dev/quick-start#jumpstart">
+                                    <span class="text-l"><span class="padding-xs background-secondary radius-s border">1.1</span> Jumpstart</span>
+                                    <p class="text-s">
+                                        Start and open projects, control
+                                        settings and get help.
+                                    </p>
+                                </a>
+                            </div>
+                            <div class="padding-l border-bottom">
+                                <a href="https://guide.rnbw.dev/quick-start#do-something">
+                                    <span class="text-l"><span class="padding-xs background-secondary radius-s border">1.7</span> Do something</span>
+                                    <p class="text-s">
+                                        All of the actions
+                                        offered by rnbw.
+                                    </p>
+                                </a>
+                            </div>
+                            <div class="padding-l">
+                                <a href="https://guide.rnbw.dev/quick-start#add-something">
+                                    <span class="text-l">
+                                        <span class="padding-xs background-secondary radius-s border">1.8</span> Add something
+                                    </span>
+                                    <p class="text-s">
+                                        Add different things in different
+                                        contexts.
+                                    </p>
+                                </a>
+                            </div>
                         </div>
-                        <div
-                            class="background-primary padding-l radius-s border">
-                            <a href="#do-something">
-                                <span class="text-l">1.7 Do something</span>
-                                <p class="text-s">
-                                    All of the features and functions offered by
-                                    rnbw.
-                                </p>
-                            </a>
-                        </div>
-                        <div
-                            class="background-primary padding-l radius-s border">
-                            <a href="#add-something">
-                                <span class="text-l">1.8 Add something</span>
-                                <p class="text-s">
-                                    Add different things in different contexts.
-                                </p>
-                            </a>
-                        </div>
-                        <div
-                            class="direction-column border-left padding-l background-primary radius-s border">
-                            <a href="#code">
-                                <span class="text-l">1.5 Code</span>
+                        <div id="anim3" class=" box direction-column border-left padding-l background-primary radius-s border">
+                            <a href="https://guide.rnbw.dev/quick-start#code">
+                                <span class="text-l"><span class="padding-xs background-secondary radius-s border">1.5</span> Code</span>
                                 <p class="text-s">
                                     Edit your code. The real thing.
                                 </p>
@@ -123,3 +114,52 @@ class RnbwMap extends HTMLElement {
 }
 
 customElements.define("rnbw-map", RnbwMap);
+
+document.addEventListener("DOMContentLoaded", () => {
+    const rnbwMapElement = document.querySelector("rnbw-map");
+    const fadeInSections = rnbwMapElement.querySelectorAll("div");
+
+    // Add CSS styles
+    const style = document.createElement("style");
+    style.innerHTML = `
+    rnbw-map div {
+      opacity: 0;
+      transform: translateY(20px);
+      transition: opacity 0.5s, transform 0.5s;
+    }
+
+    rnbw-map div.is-visible {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  `;
+    document.head.appendChild(style);
+
+    const fadeInOnScroll = (entries, observer) => {
+        let delay = 0;
+
+        entries.forEach((entry) => {
+            if (!entry.isIntersecting) {
+                return;
+            }
+
+            setTimeout(() => {
+                entry.target.classList.add("is-visible");
+            }, delay);
+
+            delay += 250;
+            observer.unobserve(entry.target);
+        });
+    };
+
+    const options = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver(fadeInOnScroll, options);
+    fadeInSections.forEach((section) => {
+        observer.observe(section);
+    });
+});
